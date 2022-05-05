3d2b459c981a92c2a1ec6803443836246d04134b
EmilFrick <emil.a.frick@gmail.com>
2022-05-05 03:12:13 +0200

    task completed, description column added

--- src/Components/Footer/ContentDescription.js
+++ src/Components/Footer/ContentDescription.js
@@ -3,10 +3,17 @@ import classes from "./ContentDescription.module.css";
 const ContentDescription = () => {
   return (
     <div className={classes.column}>
-        <img src="/img/kenkatablue.jpg" />
+      <img src="/img/kenkatablue.jpg" className={classes.img} />
       <div className={classes.description}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent enim quam, ullamcorper vel tellus vitae, tempus fringilla lectus. Donec maximus justo eu nisl convallis iaculis. Aenean laoreet.
       </div>
+      <div className={classes.follow}>
+        Follow us
+        <div className={classes.decorationcontainer}>
+          <hr className={classes.decoration} />
+        </div>
+      </div>
+      <img src="/img/placeholderblue.png" />
     </div>
   );
 };
