473fea954420332bb0b9509d3b26f5049fdccd9c
EmilFrick <emil.a.frick@gmail.com>
2022-05-05 02:17:18 +0200

    kenkata logo added

--- src/Components/Footer/ContentDescription.js
+++ src/Components/Footer/ContentDescription.js
@@ -2,8 +2,11 @@ import classes from "./ContentDescription.module.css";
 
 const ContentDescription = () => {
   return (
-    <div className={classes.description}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent enim quam, ullamcorper vel tellus vitae, tempus fringilla lectus. Donec maximus justo eu nisl convallis iaculis. Aenean laoreet.
+    <div className={classes.column}>
+        <img src="/img/kenkatablue.jpg" />
+      <div className={classes.description}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent enim quam, ullamcorper vel tellus vitae, tempus fringilla lectus. Donec maximus justo eu nisl convallis iaculis. Aenean laoreet.
+      </div>
     </div>
   );
 };
