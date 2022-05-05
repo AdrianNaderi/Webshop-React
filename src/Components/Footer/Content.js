fbd8c92b9bc7dc7163f304f8ebd2411176e41603
EmilFrick <emil.a.frick@gmail.com>
2022-05-05 05:20:10 +0200

    Task #102 completed- added recent instagram

--- src/Components/Footer/Content.js
+++ src/Components/Footer/Content.js
@@ -1,6 +1,7 @@
 import classes from "./Content.module.css";
 import ContentContactInfo from "./ContentContactInfo";
 import ContentDescription from "./ContentDescription";
+import ContentRecentInstagram from "./ContentRecentInstagram";
 import ContentUsefulLinks from "./ContentUsefulLinks";
 
 const Content = () => {
@@ -14,6 +15,9 @@ const Content = () => {
       </div>
       <div className={classes.container}>
         <ContentUsefulLinks />
+      </div>
+      <div className={classes.container}>
+        <ContentRecentInstagram />
       </div>
     </>
   );
---
+++ src/Components/Footer/ContentRecentInstagram.js
@@ -0,0 +1,12 @@
+import classes from "./ContentRecentInstagram.module.css";
+
+const ContentRecentInstagram = () => {
+  return (
+    <>
+      <h3 className={classes.title}>RECENT INSTAGRAM</h3>
+      <img src="/img/recentinstagram.png" />
+    </>
+  );
+};
+
+export default ContentRecentInstagram;
