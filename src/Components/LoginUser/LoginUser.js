95cb2cb37761a3d5eb0ad9acd5a80e2f4f8e3ec9
JoyWanjiku <76406254+JoyWanjiku@users.noreply.github.com>
2022-05-06 18:01:20 +0100

    navbartop done, deleted usermodule

--- src/Components/LoginUser/LoginUser.js
+++ src/Components/LoginUser/LoginUser.js
@@ -1,52 +1,54 @@
-import { Label } from '@doist/todoist-api-typescript';
 import React, { useState } from 'react'
 import RegisterUser from '../RegisterUser/RegisterUser';
-import classes from './LoginUser.module.css';
+import classes from './LoginUser.module.css'
 
 function LoginUser({ Login, error }) {
-    const [details, setDetails] = useState({name: "", email: "", password: ""});
+    const [details, setDetails] = useState({ name: "", email: "", password: "" });
 
     const submitHandler = e => {
         e.preventDefault();
 
         Login(details);
     }
+    return (
+        <div className={classes.form}>
+            <div class={classes.container}>
+                <form onSubmit={submitHandler}>
+                    <div class={classes.column}>
+                        <div className={classes.font}>
+                            <h2 className={classes.header}>LOGIN</h2>
+                        </div>
+                        {(error !== "") ? (<div className={classes.error}>{error}</div>) : ""}
+                        <label className={classes.labelgroup} htmlFor='name'>Name:</label>
+                        <div className={classes.formgroup}>
+                            <input type="text" name='name' id='name' onChange={e => setDetails({ ...details, name: e.target.value })} value={details.name} />
+                        </div>
+
+                        <label className={classes.labelgroup} htmlFor='email'>Email:</label>
+                        <div className={classes.formgroup}>
+                            <input type="email" name='email' id='email' onChange={e => setDetails({ ...details, email: e.target.value })} value={details.email} />
+                        </div>
+
+                            <label className={classes.labelgroup} htmlFor='password'>Password:</label>
+                        <div className={classes.formgroup}>
+                            <input type="password" name='password' id='password' onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} />
+                        </div>
+
+                        <button class={classes.button}input type="submit" value="Login">LOG IN</button>
+                        
+                        <div className={classes.footer}>
+                        <input className={classes.checkbox} type="checkbox" value="RememberMe" id="rememberMe"/> <label  className={classes.checkbox}for="rememberMe">Remember me</label>
+                        <a href="url" className={classes.link}> Lost your password?</a>
+                        <img src="/img/socials.png" className={classes.social}></img>
+                        </div>
+                    </div>
+                </form>
+                <RegisterUser />
+            </div>
+        </div>
 
 
-  return (
-    <div className={classes.form}>
-        <div className={classes.container}>
-    <form onSubmit={submitHandler}>
-            <div className={classes.row}>
-            <div className={classes.column}>
-            <div className={classes.comp1}>   
-          <h2>LOGIN</h2>
-          {(error !== "") ? ( <div className={classes.error}>{error}</div>) :""}
-          <div className={classes.formgroup}>
-                  <label htmlFor='name'>Name</label>
-                  <input type="text"  name='name' id='name' onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
-              </div>
-              <div className={classes.formgroup}>
-                  <label htmlFor='email'>Email</label>
-                  <input type="email" name='email' id='email' onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
-              </div>
-              <div className={classes.formgroup}>
-                  <label htmlFor='password'>Password</label>
-                  <input type="password"  name='password' id='password' onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-              </div>
-              <button input type="submit" className={classes.buttontext} value="Login">LOG IN</button>
-    </div>
-    </div>
-    </div>
-
-          <RegisterUser/>
-      </form>
-      
-      </div>
-      </div>
-
-
-  )
+    )
 }
 
 export default LoginUser
--- src/Components/Navbar/NavbarTop.js
+++ src/Components/Navbar/NavbarTop.js
@@ -3,12 +3,29 @@ import classes from "./NavbarTop.module.css";
 
 const NavbarTop = () => {
   return <>
-  <div className={classes.top}>
-    <div className={classes.images}>
-  <img src="/img/topleft.png" className={classes.imgleft}/>
-  <img src="/img/topright.png" class={classes.imgright} />
-</div>
-</div>
+    <div className={classes.top}>
+      <div className={classes.dropdown}>
+        <select className={classes.language}>
+          <option value="language">ENG</option>
+          <option value="language">SVE</option>
+        </select>
+
+        <select className={classes.currency}>
+          <option value="currency">USD</option>
+          <option value="currency">SEK</option>
+        </select>
+      </div>
+
+      <div className={classes.topright}>
+        <p><a href="#" className={classes.right}>Newsletter</a></p>
+        {<p className={classes.space}>space</p>}
+        <p><a href="#" className={classes.right}>Contact Us</a></p>
+        <p className={classes.space}>space</p>
+        <p><a href="#" className={classes.right}>FAQs</a></p>
+        <p className={classes.space}>space</p>
+        <p><a href="#" className={classes.right}>[] [] [] [] []</a></p>
+      </div>
+    </div>
   </>;
 };
 
