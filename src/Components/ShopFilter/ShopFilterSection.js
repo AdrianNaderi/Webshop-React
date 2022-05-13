726ff98d11baf427acb7fb11c5b73f49828ac1ff
EmilFrick <emil.a.frick@gmail.com>
2022-05-13 09:05:43 +0200

    usage of newly added dummydata

--- src/Components/ShopFilter/ShopFilterSection.js
+++ src/Components/ShopFilter/ShopFilterSection.js
@@ -1,10 +1,12 @@
 import StringFilter from "./StringFilter";
-import { categories } from "../../SD/placeholders";
+import { categories, colors, sizes } from "../../SD/placeholders";
 
 const ShopFilterSection = () => {
   return (
     <>
-      <StringFilter title={"Product Categories"} items={categories}/>
+      <StringFilter title={"Product Categories"} items={categories} />
+      <StringFilter title={"Filter by Color"} items={colors} />
+      <StringFilter title={"Filter by Size"} items={sizes} />
     </>
   );
 };
