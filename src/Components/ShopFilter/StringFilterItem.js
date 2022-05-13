f5d9cbd5530961c667b9d57ff0b756a7dfd84a26
EmilFrick <emil.a.frick@gmail.com>
2022-05-13 09:04:41 +0200

    refactored active selection for StringFilterItem.

--- src/Components/ShopFilter/StringFilterItem.js
+++ src/Components/ShopFilter/StringFilterItem.js
@@ -2,10 +2,10 @@ import FilterBadge from "./FilterBadge";
 import classes from "./StringFilterItem.module.css";
 
 const StringFilterItem = (props) => {
-    const activeStyle = props.active ? 'red' : 'black'
-  
-    return (
-    <li className={`${classes.row}`} onClick={() => props.onActive(props.item.name)} style={{color: activeStyle}}>
+  const activeStyle = props.active ? classes.active : classes.notactive;
+
+  return (
+    <li className={`${classes.row} ${activeStyle}`} onClick={() => props.onActive(props.item.name)}>
       <span>{props.item.name}</span>
       <FilterBadge quantity={props.item.quantity} />
     </li>
